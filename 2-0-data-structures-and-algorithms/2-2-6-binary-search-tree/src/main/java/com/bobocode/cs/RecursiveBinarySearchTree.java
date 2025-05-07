cd90fba373023394dd263b537897204af1674a3a
Maxym <maxym0198@gmail.com>
2025-05-08 00:12:06 +0300

    Exercise 2-2-6

--- 2-0-data-structures-and-algorithms/2-2-6-binary-search-tree/src/main/java/com/bobocode/cs/RecursiveBinarySearchTree.java
+++ 2-0-data-structures-and-algorithms/2-2-6-binary-search-tree/src/main/java/com/bobocode/cs/RecursiveBinarySearchTree.java
@@ -1,8 +1,10 @@
 package com.bobocode.cs;
 
-import com.bobocode.util.ExerciseNotCompletedException;
-
 import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static java.lang.Math.max;
+import static java.util.Objects.requireNonNull;
 
 /**
  * {@link RecursiveBinarySearchTree} is an implementation of a {@link BinarySearchTree} that is based on a linked nodes
@@ -17,33 +19,114 @@ import java.util.function.Consumer;
  * @author Maksym Stasiuk
  */
 public class RecursiveBinarySearchTree<T extends Comparable<T>> implements BinarySearchTree<T> {
+    private Node<T> root;
+    private int size;
 
     public static <T extends Comparable<T>> RecursiveBinarySearchTree<T> of(T... elements) {
-        throw new ExerciseNotCompletedException();
+        RecursiveBinarySearchTree<T> tree = new RecursiveBinarySearchTree<>();
+        Stream.of(elements).forEach(tree::insert);
+        return tree;
     }
 
     @Override
     public boolean insert(T element) {
-        throw new ExerciseNotCompletedException();
+        requireNonNull(element);
+        boolean isInserted = insertElement(element);
+        if (isInserted) {
+            size++;
+        }
+        return isInserted;
+    }
+
+    private boolean insertElement(T element) {
+        if (root == null) {
+            root = new Node<>(element);
+            return true;
+        }
+        return insertIntoSubTree(element, root);
+    }
+
+    private boolean insertIntoSubTree(T element, Node<T> node) {
+        if (node.value.compareTo(element) > 0) {
+            return insertIntoSubTreeLeft(element, node);
+        } else if (node.value.compareTo(element) < 0) {
+            return insertIntoSubTreeRight(element, node);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean insertIntoSubTreeLeft(T element, Node<T> node) {
+        if (node.left != null) {
+            return insertIntoSubTree(element, node.left);
+        }
+        node.left = new Node<>(element);
+        return true;
+    }
+
+    private boolean insertIntoSubTreeRight(T element, Node<T> node) {
+        if (node.right != null) {
+            return insertIntoSubTree(element, node.right);
+        }
+        node.right = new Node<>(element);
+        return true;
     }
 
     @Override
     public boolean contains(T element) {
-        throw new ExerciseNotCompletedException();
+        requireNonNull(element);
+        return contains(element, root);
+    }
+
+    private boolean contains(T element, Node<T> node) {
+        if (node == null) {
+            return false;
+        } else if (node.value.compareTo(element) > 0) {
+            return contains(element, node.left);
+        } else if (node.value.compareTo(element) < 0) {
+            return contains(element, node.right);
+        } else {
+            return true;
+        }
     }
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException();
+        return size;
     }
 
     @Override
     public int depth() {
-        throw new ExerciseNotCompletedException();
+        return root != null ? depth(root) - 1 : 0;
+    }
+
+    private int depth(Node<T> node) {
+        if (node == null) {
+            return 0;
+        }
+        return max(depth(node.left), depth(node.right)) + 1;
     }
 
     @Override
     public void inOrderTraversal(Consumer<T> consumer) {
-        throw new ExerciseNotCompletedException();
+        inOrderTraversal(root, consumer);
+    }
+
+    private void inOrderTraversal(Node<T> node, Consumer<T> consumer) {
+        if (node != null) {
+            inOrderTraversal(node.left, consumer);
+            consumer.accept(node.value);
+            inOrderTraversal(node.right, consumer);
+        }
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node(T value) {
+            this.value = value;
+        }
     }
 }
