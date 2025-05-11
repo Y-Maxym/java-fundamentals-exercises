01813815fd37f86a3e9330487d4a50e5c232df88
Maxym <maxym0198@gmail.com>
2025-05-11 15:50:08 +0300

    Exercise 3-6-2

--- 3-0-java-core/3-6-2-file-stats/src/main/java/com/bobocode/se/FileStats.java
+++ 3-0-java-core/3-6-2-file-stats/src/main/java/com/bobocode/se/FileStats.java
@@ -1,12 +1,61 @@
 package com.bobocode.se;
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * {@link FileStats} provides an API that allow to get character statistic based on text file. All whitespace characters
  * are ignored.
  */
 public class FileStats {
+
+    private final Map<Character, Long> characterStat;
+
+    private FileStats(String fileName) {
+        Path filePath = createFilePathByFileName(fileName);
+        try (Stream<String> strings = readFileByFilePath(filePath)) {
+            characterStat = countCharacters(strings);
+        }
+    }
+
+    private Path createFilePathByFileName(String fileName) {
+        try {
+            URL resource = getClass().getClassLoader().getResource(fileName);
+            if (resource == null) {
+                throw new FileStatsException("File not found");
+            }
+            return Path.of(resource.toURI());
+        } catch (URISyntaxException e) {
+            throw new FileStatsException("Uri syntax exception", e);
+        }
+    }
+
+    private Map<Character, Long> countCharacters(Stream<String> strings) {
+        return strings
+                .map(s -> s.replaceAll("\\s+", ""))
+                .flatMapToInt(String::chars)
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
+    }
+
+    private Stream<String> readFileByFilePath(Path filePath) {
+        try {
+            return Files.lines(filePath);
+        } catch (IOException e) {
+            throw new FileStatsException("Error reading file", e);
+        }
+    }
+
     /**
      * Creates a new immutable {@link FileStats} objects using data from text file received as a parameter.
      *
@@ -14,7 +63,7 @@ public class FileStats {
      * @return new FileStats object created from text file
      */
     public static FileStats from(String fileName) {
-        throw new ExerciseNotCompletedException(); //todo
+        return new FileStats(fileName);
     }
 
     /**
@@ -24,7 +73,7 @@ public class FileStats {
      * @return a number that shows how many times this character appeared in a text file
      */
     public int getCharCount(char character) {
-        throw new ExerciseNotCompletedException(); //todo
+        return characterStat.get(character).intValue();
     }
 
     /**
@@ -33,7 +82,11 @@ public class FileStats {
      * @return the most frequently appeared character
      */
     public char getMostPopularCharacter() {
-        throw new ExerciseNotCompletedException(); //todo
+        return characterStat.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElseThrow();
+
     }
 
     /**
@@ -43,6 +96,6 @@ public class FileStats {
      * @return {@code true} if this character has appeared in the text, and {@code false} otherwise
      */
     public boolean containsCharacter(char character) {
-        throw new ExerciseNotCompletedException(); //todo
+        return characterStat.containsKey(character);
     }
 }
