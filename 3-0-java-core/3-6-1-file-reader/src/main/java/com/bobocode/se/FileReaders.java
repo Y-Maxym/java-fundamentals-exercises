efa17328430554b27bf87e3b170656790c358c60
Maxym <maxym0198@gmail.com>
2025-05-11 14:21:06 +0300

    Exercise 3-6-1

--- 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
+++ 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
@@ -1,6 +1,15 @@
 package com.bobocode.se;
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * {@link FileReaders} provides an API that allow to read whole file into a {@link String} by file name.
@@ -14,6 +23,20 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new ExerciseNotCompletedException(); //todo
+        try (Stream<String> stream = Files.lines(createPathFromFileName(fileName))) {
+            return stream
+                    .collect(Collectors.joining("\n"));
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Path createPathFromFileName(String fileName) throws FileNotFoundException, URISyntaxException {
+        requireNonNull(fileName);
+        URL resource = FileReaders.class.getClassLoader().getResource(fileName);
+        if (resource == null) {
+            throw new FileNotFoundException();
+        }
+        return Path.of(resource.toURI());
     }
 }
