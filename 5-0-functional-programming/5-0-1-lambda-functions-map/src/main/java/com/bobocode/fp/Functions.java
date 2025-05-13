3d97c3e0e785293093ffb02e5baa7a457787dc8b
Maxym <maxym0198@gmail.com>
2025-05-13 19:20:41 +0300

    Exercise 5-0-1

--- 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
+++ 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
@@ -6,7 +6,7 @@ package com.bobocode.fp;
  * <p>
  * TODO: implement a method and verify it by running {@link FunctionsTest}
  * <p>
- * TODO: if you find this exercise valuable and you want to get more like it, <a href="https://www.patreon.com/bobocode"> 
+ * TODO: if you find this exercise valuable and you want to get more like it, <a href="https://www.patreon.com/bobocode">
  *     please support us on Patreon</a>
  *
  * @author Taras Boychuk
@@ -28,7 +28,11 @@ public class Functions {
     public static FunctionMap<Integer, Integer> intFunctionMap() {
         FunctionMap<Integer, Integer> intFunctionMap = new FunctionMap<>();
 
-        // todo: according to the javadoc add functions using lambda expression
+        intFunctionMap.addFunction("abs", Math::abs);
+        intFunctionMap.addFunction("sgn", n -> Float.valueOf(Math.signum(n)).intValue());
+        intFunctionMap.addFunction("increment", n -> n + 1);
+        intFunctionMap.addFunction("decrement", n -> n - 1);
+        intFunctionMap.addFunction("square", n -> n * n);
 
         return intFunctionMap;
     }
