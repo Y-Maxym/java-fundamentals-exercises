9aa211085a0bed60864401e9b380dc9698107da2
Maxym <maxym0198@gmail.com>
2025-05-16 20:21:25 +0300

    Exercise 6-1-1

--- 6-0-test-driven-development/6-1-1-stack/src/main/java/com/bobocode/tdd/LinkedStack.java
+++ 6-0-test-driven-development/6-1-1-stack/src/main/java/com/bobocode/tdd/LinkedStack.java
@@ -1,25 +1,53 @@
 package com.bobocode.tdd;
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import java.util.NoSuchElementException;
+
+import static java.util.Objects.requireNonNull;
 
 public class LinkedStack<T> implements Stack<T> {
+    private Node<T> head;
+    private int size;
+
     @Override
     public void push(T element) {
-        throw new ExerciseNotCompletedException(); // todo
+        requireNonNull(element);
+        head = new Node<>(element, head);
+        size++;
     }
-    
+
     @Override
     public T pop() {
-        throw new ExerciseNotCompletedException(); // todo
+        if (size == 0) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+
+        T value = head.value;
+        head = head.next;
+        size--;
+        return value;
     }
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException(); // todo
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new ExerciseNotCompletedException(); // todo
+        return size == 0;
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
     }
 }
--- 6-0-test-driven-development/6-1-1-stack/src/test/java/com/bobocode/tdd/StackTest.java
+++ 6-0-test-driven-development/6-1-1-stack/src/test/java/com/bobocode/tdd/StackTest.java
@@ -1,4 +1,127 @@
 package com.bobocode.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class StackTest {
-}
+
+    private Stack<Integer> stack;
+
+    @BeforeEach
+    void init() {
+        stack = new LinkedStack<>();
+    }
+
+    @Test
+    void givenEmptyStack_whenCallSize_thenSizeIsZero() {
+        // when
+        int size = stack.size();
+
+        // then
+        assertThat(size).isEqualTo(0);
+    }
+
+    @Test
+    void givenEmptyStack_whenCallIsEmpty_thenReturnTrue() {
+        // when
+        boolean isEmpty = stack.isEmpty();
+
+        // then
+        assertThat(isEmpty).isTrue();
+    }
+
+    @Test
+    void givenElement_whenPush_thenSizeIsNotZero() {
+        // given
+        int element = 42;
+
+        // when
+        stack.push(element);
+
+        // then
+        assertThat(stack.size()).isEqualTo(1);
+    }
+
+    @Test
+    void givenNull_whenPush_thenExceptionIsThrown() {
+        // when && then
+        assertThatThrownBy(() -> stack.push(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void givenElement_whenCallPushAndSize_thenSizeIsGreaterZero() {
+        // given
+        int element = 0;
+
+        // when
+        stack.push(element);
+        int size = stack.size();
+
+        // then
+        assertThat(size).isEqualTo(1);
+    }
+
+    @Test
+    void givenElement_whenCallPushAndIsEmpty_thenReturnFalse() {
+        // given
+        int element = 42;
+
+        // when
+        stack.push(element);
+        boolean isEmpty = stack.isEmpty();
+
+        // then
+        assertThat(isEmpty).isFalse();
+    }
+
+    @Test
+    void givenElement_whenPushAndPop_thenSameElementIsReturned() {
+        // given
+        int element = 42;
+
+        // when
+        stack.push(element);
+        int popped = stack.pop();
+
+        // then
+        assertThat(popped).isEqualTo(element);
+    }
+
+    @Test
+    void givenEmptyStack_whenPop_thenThrowException() {
+        // when && then
+        assertThatThrownBy(() -> stack.pop())
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("Stack is empty");
+    }
+
+    @Test
+    void givenMultiplePushAndPop_whenCheckSize_thenSizeIsCorrect() {
+        // given
+        assertThat(stack.size()).isZero();
+
+        // when
+        stack.push(1);
+        int sizeAfterFirstPush = stack.size();
+
+        stack.push(2);
+        int sizeAfterSecondPush = stack.size();
+
+        stack.pop();
+        int sizeAfterFirstPop = stack.size();
+
+        stack.pop();
+        int sizeAfterSecondPop = stack.size();
+
+        // then
+        assertThat(sizeAfterFirstPush).isEqualTo(1);
+        assertThat(sizeAfterSecondPush).isEqualTo(2);
+        assertThat(sizeAfterFirstPop).isEqualTo(1);
+        assertThat(sizeAfterSecondPop).isZero();
+    }}
