180ae672d90abdfc93b89b3858edf7b82be46f23
Maxym <maxym0198@gmail.com>
2025-05-18 18:35:54 +0300

    Exercise 6-1-3

--- 6-0-test-driven-development/6-1-3-binary-search-tree/src/main/java/com/bobocode/tdd/RecursiveBinarySearchTree.java
+++ 6-0-test-driven-development/6-1-3-binary-search-tree/src/main/java/com/bobocode/tdd/RecursiveBinarySearchTree.java
@@ -1,37 +1,122 @@
 package com.bobocode.tdd;
 
-import com.bobocode.util.ExerciseNotCompletedException;
-
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
-public class RecursiveBinarySearchTree<T extends Comparable> implements BinarySearchTree<T> {
+import static java.lang.Math.max;
+import static java.util.Objects.requireNonNull;
 
-    public static <T extends Comparable> RecursiveBinarySearchTree<T> of(T... elements) {
-        throw new ExerciseNotCompletedException();
+public class RecursiveBinarySearchTree<T extends Comparable<? super T>> implements BinarySearchTree<T> {
+    private Node<T> root;
+    private int size;
+
+    @SafeVarargs
+    public static <T extends Comparable<? super T>> RecursiveBinarySearchTree<T> of(T... elements) {
+        RecursiveBinarySearchTree<T> tree = new RecursiveBinarySearchTree<>();
+        Stream.of(elements).forEach(tree::insert);
+        return tree;
     }
 
     @Override
     public boolean insert(T element) {
-        throw new ExerciseNotCompletedException();
+        requireNonNull(element);
+        if (root == null) {
+            return insertRoot(element);
+        }
+        return insert(element, root);
+    }
+
+    private boolean insertRoot(T element) {
+        root = new Node<>(element);
+        size++;
+        return true;
+    }
+
+    private boolean insert(T element, Node<T> node) {
+        if (node.value.compareTo(element) == 0) {
+            return false;
+        } else if (node.value.compareTo(element) < 0) {
+            return insertRightLeaf(element, node);
+        } else {
+            return insertLeftLeaf(element, node);
+        }
+    }
+
+    private boolean insertLeftLeaf(T element, Node<T> node) {
+        if (node.left == null) {
+            node.left = new Node<>(element);
+            size++;
+            return true;
+        } else {
+            return insert(element, node.left);
+        }
+    }
+
+    private boolean insertRightLeaf(T element, Node<T> node) {
+        if (node.right == null) {
+            node.right = new Node<>(element);
+            size++;
+            return true;
+        } else {
+            return insert(element, node.right);
+        }
     }
 
     @Override
     public boolean contains(T element) {
-        throw new ExerciseNotCompletedException();
+        requireNonNull(element);
+        return contains(element, root);
+    }
+
+    private boolean contains(T element, Node<T> node) {
+        if (node == null) {
+            return false;
+        } else if (node.value.compareTo(element) > 0) {
+            return contains(element, node.left);
+        } else if (node.value.compareTo(element) < 0) {
+            return contains(element, node.right);
+        } else {
+            return true;
+        }
     }
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException();
+        return size;
     }
 
     @Override
     public int depth() {
-        throw new ExerciseNotCompletedException();
+        return root == null ? 0 : depth(root) - 1;
+    }
+
+    private int depth(Node<T> root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + max(depth(root.left), depth(root.right));
     }
 
     @Override
     public void inOrderTraversal(Consumer<T> consumer) {
-        throw new ExerciseNotCompletedException();
+        inOrderTraversal(consumer, root);
+    }
+
+    private void inOrderTraversal(Consumer<T> consumer, Node<T> node) {
+        if (node != null) {
+            inOrderTraversal(consumer, node.left);
+            consumer.accept(node.value);
+            inOrderTraversal(consumer, node.right);
+        }
+    }
+
+    private static class Node<T extends Comparable<? super T>> {
+        private final T value;
+        private Node<T> left;
+        private Node<T> right;
+
+        private Node(T value) {
+            this.value = value;
+        }
     }
 }
--- 6-0-test-driven-development/6-1-3-binary-search-tree/src/test/java/com/bobocode/tdd/BinarySearchTreeTest.java
+++ 6-0-test-driven-development/6-1-3-binary-search-tree/src/test/java/com/bobocode/tdd/BinarySearchTreeTest.java
@@ -1,5 +1,198 @@
 package com.bobocode.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class BinarySearchTreeTest {
 
+    private BinarySearchTree<Integer> tree;
+
+    @BeforeEach
+    void init() {
+        tree = new RecursiveBinarySearchTree<>();
+    }
+
+    @Test
+    void givenEmptyTree_whenCallSize_thenSizeIsZero() {
+        // when
+        int size = tree.size();
+
+        // then
+        assertThat(size).isEqualTo(0);
+    }
+
+    @Test
+    void givenEmptyTree_whenInsert_thenSizeIsIncreased() {
+        // when
+        tree.insert(42);
+
+        // then
+        assertThat(tree.size()).isEqualTo(1);
+    }
+
+    @Test
+    void givenEmptyTree_whenInsert_thenReturnsTrue() {
+        // when
+        boolean isInserted = tree.insert(42);
+
+        // then
+        assertThat(isInserted).isTrue();
+    }
+
+    @Test
+    void givenEmptyTree_whenInsertNullElement_thenExceptionIsThrown() {
+        // when && then
+        assertThatThrownBy(() -> tree.insert(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void givenElements_whenCallOf_thenFilledTreeReturns() {
+        // given
+        Integer[] elements = {1, 2, 3, 4, 5};
+
+        // when
+        RecursiveBinarySearchTree<Integer> newTree = RecursiveBinarySearchTree.of(elements);
+
+        // then
+        assertThat(newTree.size()).isEqualTo(5);
+    }
+
+    @Test
+    void givenNonEmptyTree_whenInsert_thenReturnsFalse() {
+        // given
+        tree = RecursiveBinarySearchTree.of(1, 2, 3, 4, 5, 42);
+
+        // when
+        boolean isInserted = tree.insert(42);
+
+        // then
+        assertThat(isInserted).isFalse();
+        assertThat(tree.size()).isEqualTo(6);
+    }
+
+    @Test
+    void givenNonEmptyTree_whenInsert_thenReturnsTrue() {
+        // given
+        tree = RecursiveBinarySearchTree.of(1, 2, 3, 4, 5);
+
+        // when
+        boolean isInserted = tree.insert(42);
+
+        // then
+        assertThat(isInserted).isTrue();
+        assertThat(tree.size()).isEqualTo(6);
+    }
+
+    @Test
+    void givenEmptyTree_whenContainsNullElement_thenExceptionIsThrown() {
+        // when && then
+        assertThatThrownBy(() -> tree.contains(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void givenNonEmptyTree_whenContains_thenReturnTrue() {
+        // given
+        tree = RecursiveBinarySearchTree.of(1, 2, 3, 4, 5);
+
+        // when
+        boolean isPresent = tree.contains(3);
+
+        // then
+        assertThat(isPresent).isTrue();
+    }
+
+    @Test
+    void givenNonEmptyTree_whenContains_thenReturnFalse() {
+        // given
+        tree = RecursiveBinarySearchTree.of(1, 2, 3, 4, 5);
+
+        // when
+        boolean isPresent = tree.contains(42);
+
+        // then
+        assertThat(isPresent).isFalse();
+    }
+
+    @Test
+    void givenEmptyTree_whenContains_thenReturnFalse() {
+        // when
+        boolean isPresent = tree.contains(42);
+
+        // then
+        assertThat(isPresent).isFalse();
+    }
+
+    @Test
+    void givenEmptyTree_whenDepth_thenReturnsZero() {
+        // when
+        int depth = tree.depth();
+
+        // then
+        assertThat(depth).isEqualTo(0);
+    }
+
+    @Test
+    void givenOneElementTree_whenDepth_thenReturnsZero() {
+        // given
+        tree = RecursiveBinarySearchTree.of(1);
+
+        // when
+        int depth = tree.depth();
+
+        // then
+        assertThat(depth).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDataForDepth")
+    void givenNonEmptyTree_whenDepth_thenReturnsCorrectDepth(Integer[] elements, int expectedDepth) {
+        // given
+        tree = RecursiveBinarySearchTree.of(elements);
+
+        // when
+        int depth = tree.depth();
+
+        // then
+        assertThat(depth).isEqualTo(expectedDepth);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDataForInOrderTraversal")
+    void givenNonEmptyTree_whenInOrderTraversal_thenBehaviorIsExpected(Integer[] elements, String expectedResult) {
+        // given
+        tree = RecursiveBinarySearchTree.of(elements);
+        StringBuilder sb = new StringBuilder();
+
+        // when
+        tree.inOrderTraversal(sb::append);
+
+        // then
+        assertThat(sb.toString()).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> provideDataForDepth() {
+        return Stream.of(
+                Arguments.of(new Integer[]{1, 2, 3, 4, 5}, 4),
+                Arguments.of(new Integer[]{3, 1, 4, 5}, 2),
+                Arguments.of(new Integer[]{3, 2, 1, 4}, 2)
+        );
+    }
+
+    private static Stream<Arguments> provideDataForInOrderTraversal() {
+        return Stream.of(
+                Arguments.of(new Integer[]{1, 2, 3, 4, 5}, "12345"),
+                Arguments.of(new Integer[]{3, 1, 4, 5}, "1345"),
+                Arguments.of(new Integer[]{3, 2, 1, 4}, "1234")
+        );
+    }
 }
